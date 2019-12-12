031ce866236eedcf97e2cd9ef0deb0a9a24e7c76
Krzysztof Szachna-Krause <kszk@gmx.us>
2019-12-12 08:32:17 +0100

    hooks introduced

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,64 +1,50 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export class Search extends Component {
-  state = {
-    text: ""
-  };
+const Search = ({ searchUsers, showIt, clearUsers, setAlert }) => {
+  const [text, setText] = useState("");
 
-  static propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showIt: PropTypes.number.isRequired,
-    setAlert: PropTypes.func.isRequired
-  };
-  onChange = e => {
-    this.setState({
-      //text: e.target.value
-      [e.target.name]: e.target.value
-    });
-  };
+  const onChange = e => setText(e.target.value);
 
-  onSubmit = e => {
+  const onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("Please enter something to search for...", "light");
+    if (text === "") {
+      setAlert("Please enter something to search for...", "light");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({
-        text: ""
-      });
+      searchUsers(text);
+      setText("");
     }
   };
-  render() {
-    console.log(this.props.showIt);
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input
-            type="text"
-            name="text"
-            placeholder="Search for users..."
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {this.props.showIt !== 0 ? (
-          <button
-            className="btn btn-light btn-block"
-            onClick={this.props.clearUsers}
-          >
-            Clear
-          </button>
-        ) : null}
-      </div>
-    );
-  }
-}
 
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search for users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showIt !== 0 ? (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      ) : null}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showIt: PropTypes.number.isRequired,
+  setAlert: PropTypes.func.isRequired
+};
 export default Search;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,29 +4,6 @@ import { Spinner } from "../layout/Spinner";
 import PropTypes from "prop-types";
 
 const Users = props => {
-  // state = {
-  //   users: [
-  //     {
-  //       id: 1,
-  //       login: "mojombo",
-  //       avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-  //       html_url: "https://github.com/mojombo"
-  //     },
-  //     {
-  //       id: 2,
-  //       login: "defunkt",
-  //       avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-  //       html_url: "https://github.com/defunkt"
-  //     },
-  //     {
-  //       id: 3,
-  //       login: "pjhyett",
-  //       avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-  //       html_url: "https://github.com/defunkt"
-  //     }
-  //   ]
-  // };
-
   if (props.loading) {
     return <Spinner />;
   } else {
